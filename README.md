7d4b10b389bf76f835f0f5bf6d5bce83d889f6f7
YuliiaZas <julia.kv101@gmail.com>
2021-07-26 12:33:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-kffhqr
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-kffhqr)
---
+++ index.ts
@@ -0,0 +1,74 @@
+// 1. Add typings/access modifiers to the fruitBasket constant
+enum Fruit {
+  BANANA = 'banana',
+  ORANGE = 'orange',
+  KIWI = 'kiwi',
+  APPLE = 'apple'
+}
+const fruitBasket: FruitBasket = {
+  banana: 2,
+  orange: 3,
+  kiwi: 2,
+  apple: 3
+};
+type FruitBasket = {
+  [key in Fruit]: number;
+}
+
+// 2. Add typings/access modifiers to the Person class
+class Person {
+  name: string;
+  gender: string;
+  age: number;
+  likes: string[];
+  public constructor(name: string, gender: string, age: number, likes: string[]) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.likes = likes;
+  }
+
+  public introduce() {
+    const { name, gender, age, likes } = this;
+    const goodLookingMap: Map<string, string> = new Map([['male', 'handsome'], ['female', 'cute']]);
+    return `
+      Hello, I'm ${name}, ${age} years old, I like: ${likes.join(', ')}. 
+      As you can see, I'm quite ${goodLookingMap.get(gender)} too!
+    `;
+  }
+}
+
+const Dima = new Person('Dima', 'male', 22, ['video games', 'martial arts']);
+
+// 3. Add typings/access modifiers to MovieService class
+interface Logger {
+  log: (err: Error) => void;
+}
+
+class MovieService {
+  logger: Logger;
+  constructor(logger: Logger) {
+    this.logger = logger;
+  }
+  public getMovies() {
+    return Promise.resolve(['Jaws', 'Spider-Man']).catch(err => {
+      this.logger.log(err);
+      return [];
+    });
+  }
+}
+
+class LoggerOne implements Logger {
+  public log(err: Error) {
+    console.log('sending logs to log storage 1', err);
+  }
+}
+class LoggerTwo implements Logger {
+  public log(err: Error) {
+    console.log('sending logs to log storage 2', err);
+  }
+}
+
+const movieService1 = new MovieService(new LoggerOne());
+const movieService2 = new MovieService(new LoggerTwo());
+
